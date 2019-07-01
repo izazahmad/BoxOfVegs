307626f9f4b177bed32ed35118935503377fbd17
Izazahmad Shaikh <shekhizaz@gmail.com>
2019-07-01 11:44:15 +0100

    initiated

---
+++ BoxOfVegs.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BoxOfVegs.Web.Startup))]
+namespace BoxOfVegs.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
